038e5bfcc7c866052a9bde2baa17aff29c1ae737
Franco <francograndedela@gmail.com>
2025-03-15 19:51:07 -0300

    creando link a producto por id con sus propiedades, siguiente a crear: DELETE

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -21,14 +21,40 @@ router.post('/', async (req, res) => {
     })
 
 
-    // read
+// read
+
+//Ver un producto
+router.get('/:cod', async (req, res) => {
+    try{
+        const product = await ProductModel.findOne({cod: req.params.cod}).lean();
+        if(!product){
+            return res.render('error',{error: "Producto no encontrado"})
+        }
+        res.render('product', {product});
+    }catch(error){
+        console.error(error);
+        return res.render('error',{error: "Error al obtener el producto solicitado"})
+    }
+})
+
+// ver productos en index
     router.get('/', async (req, res) => {
         try{
-            let products = await ProductModel.find();
-            res.render('products', {products : products.map( product => product.toObject())});
+            let products = await ProductModel.find().lean;
+            res.redirect('/');
         }catch(error){
             return res.json({message: error})
         }
+    })
+
+    //borrar producto por ID
+
+    router.delete('/:pid', async (req, res) => {
+        const productoElminiar = await ProductModel.findByIdAndDelete(req.params.pid);
+        if (!productoElminiar) {
+            return res.status(404).json({ message: 'Producto no encontrado' });
+        }
+        res.json({ message: 'Producto eliminado exitosamente' })
     })
 
     export default router;
